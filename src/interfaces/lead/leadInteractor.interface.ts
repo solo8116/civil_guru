1b635513a4ce7e54bd6647c05269002c1c71b5c6
Sarthak Garg <gr3y.m47t3r@gmail.com>
2025-09-03 16:25:07 +0530

    fix: add missing filterLead method to ILeadInteractor

--- src/interfaces/lead/leadInteractor.interface.ts
+++ src/interfaces/lead/leadInteractor.interface.ts
@@ -1,4 +1,4 @@
-import { CreateLeadDto } from '../../common/dtos';
+import { CreateLeadDto, LeadFilterDto } from '../../common/dtos';
 import { LeadEntity } from '../../entites';
 import { IPaginatedData } from '../paginatedData';
 import { IResponse } from '../response';
@@ -6,4 +6,7 @@ import { IResponse } from '../response';
 export interface ILeadInteractor {
   createLead(dto: CreateLeadDto): Promise<IResponse>;
   getAllLeads(page?: number): Promise<IResponse<IPaginatedData<LeadEntity>>>;
+  filterLead(
+    dto: LeadFilterDto,
+  ): Promise<IResponse<IPaginatedData<LeadEntity>>>;
 }
